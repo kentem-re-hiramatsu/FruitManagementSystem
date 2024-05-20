eba7bde9439c4a60490494f94bba2bfc6a2ee5d8
re-hiramatsu <re-hiramatsu>
2024-05-20 15:44:16 +0900

    enum

---
+++ FruitManagementSystem/Enums/FruitNameEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitManagementSystem.Enums
+{
+    public class FruitNameEnum
+    {
+        public enum FruitName
+        {
+            りんご = 1,
+            みかん,
+            バナナ,
+            グレープ
+        }
+    }
+}
---
+++ FruitManagementSystem/Enums/FruitPriceEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitManagementSystem.Enums
+{
+    public class FruitPriceEnum
+    {
+        public enum FruitPrice
+        {
+            りんご = 100,
+            みかん = 80,
+            バナナ = 120,
+            グレープ = 150
+        }
+    }
+}
